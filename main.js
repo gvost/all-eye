e5dc823c804387d380b4f4bb289d6df5c353d1dd
gvost <davidjohnson1297@gmail.com>
2017-10-30 20:54:54 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var path = require('path');
+var { exec } = require('child_process');
+var { app, Menu, Tray } = require('electron');
+
+var iconPath = path.join(__dirname, 'IconTemplate@2x.png');
+
+function exitApplication (app) {
+  app.quit();
+};
+function toggleHidden () {
+  exec('defaults read com.apple.finder AppleShowAllFiles', (err, stdout, stderr) => {
+    if (stdout.toString().replace(/\n$/, '') === 'YES') {
+      exec('defaults write com.apple.finder AppleShowAllFiles NO && killall Finder', (err, stdout, stderr) => {
+        if (err) {
+          return;
+        } else {
+          return;
+        }
+      });
+    } else if (stdout.toString().replace(/\n$/, '') === 'NO') {
+      exec('defaults write com.apple.finder AppleShowAllFiles YES && killall Finder', (err, stdout, stderr) => {
+        if (err) {
+          return;
+        } else {
+          return;
+        };
+      });
+    }
+  });
+};
+app.on('ready', () => {
+  var tray = new Tray(iconPath);
+  var contextMenu = Menu.buildFromTemplate([
+    {
+      click (event) {
+        toggleHidden()
+      },
+      id: 'toggle',
+      label: 'Show/Hide',
+      type: 'normal'
+    },
+    {
+      id: 'separator',
+      label: null,
+      type: 'separator'
+    },
+    {
+      click () {
+        exitApplication(app)
+      },
+      id: 'exit',
+      label: 'Exit',
+      type: 'normal'
+    }
+  ]);
+  tray.setContextMenu(contextMenu);
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+# `▲llEye—`
+> Show hidden files, then hide them again, from the tray on macOS.
+
+Icon courtesy [Erich Grois](https://thenounproject.com/erichgrois/)
